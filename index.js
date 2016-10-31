a13770772bc435a10d68a3e73d6c4ed086895ddb
dhenkes <dominique@henkes.io>
2016-10-31 12:07:54 +0100

    Fix bug

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
 const ace = require('brace');
 
 module.exports = {
-  template: "<div class=\"ace-editor\"></div>",
+  template: "<div :style=\"{height: '300px', width: '100%'}\"></div>",
+
   props: {
     content: {
       type: String,
       required: true
     },
-    lang: String,
-    theme: String
-  },
-  data: () => {
-    return {
-      editor: null
+    lang: {
+      type: String,
+      default: 'javascript'
+    },
+    theme: {
+      type: String,
+      default: 'chrome'
     }
   },
-  mounted: () => {
+
+  data () {
+    return {
+      editor: null,
+    }
+  },
+
+  mounted () {
     const vm = this;
     let lang = vm.lang || 'javascript';
     let theme = vm.theme || 'chrome';
     let editor = vm.editor = ace.edit(vm.$el);
     editor.$blockScrolling = Infinity;
     editor.getSession().setMode('ace/mode/' + lang);
-    editor.setTheme('ace/theme' + theme);
+    editor.setTheme('ace/theme/' + theme);
+    editor.setValue(vm.content, 1);
   }
 }
